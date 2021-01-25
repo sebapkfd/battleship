31dab60605ac009d4c2531098f236e0d2a70fcae
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-01-25 00:46:37 -0300

    Added link to Repository

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Title from './components/Title';
 import Board from './components/Board';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Title/>
       <Board/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <a href="https://github.com/sebapkfd/battleship" target="_blank" rel="noreferrer">
+                <img src="https://img.icons8.com/fluent/48/000000/github.png" alt="Github" id="Repo"/>
+            </a>
+        </div>
+    )
+
+}
+
+export default Footer;
