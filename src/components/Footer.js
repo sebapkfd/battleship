c271c39835f7bb85f36d8e4e143a8ef237fa431b
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-08-04 19:02:48 -0400

    Small footer changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 const Footer = () => {
     return (
-        <div className='footer'>
+        <footer>
             <a href="https://github.com/sebapkfd/battleship" target="_blank" rel="noreferrer">
                 <img src="https://img.icons8.com/fluent/48/000000/github.png" alt="Github" id="repo"/>
             </a>
-        </div>
+        </footer>
     )
 
 }
